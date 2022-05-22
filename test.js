36c6e63061613984069451c1ee8dd9efee80e672
Bilguun0229 <99532823+Bilguun0229@users.noreply.github.com>
2022-05-22 10:20:11 +0800

    test

--- test.js
+++ test.js
@@ -4,7 +4,7 @@ const date = new Date();
 const year = date.getFullYear();
 const month = date.getMonth() + 1;
 const day = date.getDate();
-const today = document.querySelector(".today");
+let today = document.querySelector(".today");
 today.innerHTML = `${year}/${month}/${day}`;
 // 8 багийн тоглолтонд 2р өдрийн зайтай хуваарь зоож өгнө.
 games = document.querySelectorAll(".date");
@@ -13,18 +13,13 @@ let gameMonth = month;
 let gameDay = day;
 for (let i = 0; i < 8; i++) {
     gameDay += 2;
-    if (gameMonth == 2 && gameDay > 28) {
+    if (gameMonth == 2 && gameDay > 28 || ((gameMonth == 4 || gameMonth == 6 || gameMonth == 9 || gameMonth == 11) && gameDay > 30) || gameDay > 31) {
         gameDay = 1;
-        gameMonth += 1;
-    } else if (gameDay > 30) {
-        if (gameMonth == 4 || gameMonth == 6 || gameMonth == 9 || gameMonth == 11) {
-            gameDay = 1;
-            gameMonth += 1;
+        gameMonth++;
+        if (gameMonth == 12) {
+            gameMonth = 1;
+            gameYear += 1;
         }
-    };
-    if (gameMonth > 12) {
-        gameMonth = 1;
-        gameYear += 1;
     }
     games[i].innerHTML = `${gameYear}/${gameMonth}/${gameDay}`
 
@@ -35,6 +30,34 @@ for (let i = 0; i < 8; i++) {
 let y = year;
 let m = month;
 let d = day;
+go = setInterval(() => {
+    d += 1;
+    if (m == 2 && d > 28 || ((m == 4 || m == 6 || m == 9 || m == 11) && d > 30) || d > 31) {
+        d = 1;
+        m++;
+        if (m == 12) {
+            m = 1;
+            y += 1;
+        }
+    }
+    today.innerHTML = `${y}/${m}/${d}`;
+    for (let i = 0; i < games.length; i++) {
+        games[i].classList.remove("active");
+        if (games.innerHTML == today.innerHTML) {
+            games[i].classList.add("active");
 
-
+        }
+    }
+    if (today.innerHTML == games[games.length - 1].innerHTML) {
+        clearInterval(go);
+    }
+}, 1000);
 // Тоглолтын функц зарлана.
+const teams = document.querySelectorAll(".teams");
+console.log(teams)
+match = () => {
+    const a = Math.floor(Math.random() * 100);
+    document.querySelector(".team1 .score").innerHTML = a;
+    const b = Math.floor(Math.random() * 100);
+    document.querySelector(".team2 .score").innerHTML = b;
+}
